2ab8d2569380617e707263b31ad15778728f0cfc
Matt Decker <matt@Matts-MBP.lan>
2022-08-16 21:01:05 -0400

    passess all tests

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,17 +2,22 @@ const Account = require('./accounts-model');
 
 exports.checkAccountPayload = (req, res, next) => {
   const { name, budget } = req.body;
-  const coercedNum = Number(budget);
+  // const coercedNum = Number(budget);
   if (req.body == null) {
     next({ status: 400, message: 'oh no'});
+    
   } else if (name === undefined || budget === undefined) {
     next({ status: 400, message: 'name and budget are required' });
+
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     next({ status: 400, message: 'name of account must be between 3 and 100'} );
-  } else if (isNaN(budget) || typeof coercedNum !== 'number' || budget === '') {
+
+  } else if (isNaN(budget) || typeof budget !== 'number') {
     next({ status: 400, message: "budget of account must be a number" });
+
   } else if (budget < 0 || budget > 1000000) {
     next({status: 400, message: 'budget of account is too large or too small' });
+
   } else {
     req.newAccount = {
       name: name.trim(),
